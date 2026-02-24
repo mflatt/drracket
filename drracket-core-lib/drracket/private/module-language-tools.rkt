9448157210c7d8a2f1d1920da1ca21d1768827f3
Robby Findler <robby@racket-lang.org>
2026-02-23 19:54:32 -0600

    adjust range-indentation so that, after indentation, it moves the insertion point to the first nonwhitespace on the line (in the case that there is an insertion point instead of a selection)

--- drracket-core-lib/drracket/private/module-language-tools.rkt
+++ drracket-core-lib/drracket/private/module-language-tools.rkt
@@ -216,6 +216,7 @@
     (mixin (text:basic<%>) (ml-tools-text<%>)
       (inherit position-paragraph paragraph-start-position
                delete insert
+               set-position paragraph-end-position get-character
                begin-edit-sequence end-edit-sequence)
       (define/public (range-indent range-indentation-function start end)
         (define substs (range-indentation-function this start end))
@@ -234,6 +235,16 @@
                      (delete pos (+ pos del-amt)))
                    (unless (equal? insert-str "")
                      (insert insert-str pos))
+                   (when (= start end)
+                     (define para-end (paragraph-end-position start-line))
+                     (let loop ([pos pos])
+                       (cond
+                         [(< pos para-end)
+                          (define c (get-character pos))
+                          (cond
+                            [(char-whitespace? c) (loop (+ pos 1))]
+                            [else (set-position pos pos)])]
+                         [else (void)])))
                    (loop (cdr substs) (add1 line))))
                (end-edit-sequence)
                #t)))
